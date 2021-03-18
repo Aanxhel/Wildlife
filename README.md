9b149794f34715e3d57ff2f58badcf52a5026f94
aanxhel <ang.core92@yahoo.com>
2021-03-18 15:05:24 -0600

    se crea base bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Wildlife
  Proyecto Wildlife
+
+
+se agrega 'npm install --save bootstrap' para vista responsive
+se agrega 'npm install --save reactstrap react react-dom'
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Base } from './Base';
+import { Base } from './resources/createBody';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+//import './custom.scss';
 
 
 ReactDOM.render(

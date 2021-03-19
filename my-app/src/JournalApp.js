aa009d1e2e0a33374c6b9d764a4373701e514903
aanxhel <ang.core92@yahoo.com>
2021-03-19 01:25:05 -0600

    recorrido a la derecha

--- my-app/src/JournalApp.js
+++ my-app/src/JournalApp.js
@@ -1,10 +1,47 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-export const JournalApp = () => {
-    return (
+function JournalApp(){
+    return(
         <div>
             <h1>JournalApp</h1>
+            <hr></hr>
+          
+
+            <nav class="navbar 
+            navbar-expand-sm 
+            bg-dark 
+            navbar-dark
+            justify-content-center">
+                
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="navbar-brand" href="#">Quienes somos?</a>
+                </li>
+                <li class="nav-item">
+                <a class="navbar-brand" href="#">Productos</a>
+                </li>
+                <li class="nav-item">
+                <a class="navbar-brand" href="#">Fuego</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="navbar-brand" href="#">Otro Item</a>
+                </li>
+
+            </ul>
+
+            </nav>
             
+
+
+
+
+
         </div>
-    )
+    );
+
 }
+
+export default JournalApp;

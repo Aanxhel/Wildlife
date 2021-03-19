a3277586e0fb2684b08b478b16c45a92f2d2a753
aanxhel <ang.core92@yahoo.com>
2021-03-19 13:18:15 -0600

    se hace importacion de imagen

--- my-app/src/JournalApp.js
+++ my-app/src/JournalApp.js
@@ -3,6 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import {Button, Alert} from 'react-bootstrap'
 
+import Caballo from './img/caballo.png'
+
 function JournalApp(){
     return(
         <div className='JournalApp'>
@@ -33,6 +35,30 @@ function JournalApp(){
             </ul>
 
             </nav>
+
+            <div class = 'container'>
+                <div class="row">
+                    <div class="col-sm-4">
+                    <h3>Column 1</h3>
+                    
+                        <img src ={Caballo} class="mx-auto d-block"></img>
+
+                    
+                </div>
+
+
+
+                <div class="col-sm-4">
+                    <h3>Column 2</h3>
+                    <p>Lorem ipsum dolor..</p>
+                </div>
+                <div class="col-sm-4">
+                    <h3>Column 3</h3>
+                    <p>Lorem ipsum dolor..</p>
+                </div>
+            </div>
+
+            </div>
             
             
             
